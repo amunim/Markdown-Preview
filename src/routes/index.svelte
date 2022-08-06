447acdfc9caac2bedf00ad4069dc39ea30d44c57
Abdul Munim <abdulmunim2005@gmail.com>
2022-08-06 09:49:53 +0300

    Added default text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,50 @@
     let parser = new Parser();
     let lexer = new Lexer();
     let showFull = 0; //0 - none, 1 - editor, 2 - preview
-    let text = "";
+    let text = `# Welcome to my Svelte Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
     let markup = "";
     $: {
         lexer = new Lexer();
